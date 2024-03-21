ce0aee97d107a7f36f3515219bb8d23823823292
Mariano Mejia <mejiamariano8.08@gmail.com>
2024-03-21 16:34:23 +0800

    add admin

---
+++ webmasterione/include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['session1'] = array ('1'=>'session1!192.168.88.1','session1@|@admin','session1#|#qJKlq6ihqpU=','session1%h1','session1^10.10.10.1','session1&Rp','session1*10','session1(1','session1)','session1=10','session1@!@enable');
